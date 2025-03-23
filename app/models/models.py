eca5590bca3771fc9ddbbf1a3a6f25cdb745f59b
ahmedaldahshoury <ahmed.aldahshoury@gmail.com>
2025-03-23 14:30:53 +0100

    Add country for city, update types of Weather params

--- app/models/models.py
+++ app/models/models.py
@@ -4,13 +4,14 @@ class City(db.Model):
     __tablename__ = 'cities'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
+    country = db.Column(db.String(50), unique=False, nullable=False)
 
 class Weather(db.Model):
     __tablename__ = 'weather'
     id = db.Column(db.Integer, primary_key=True)
     city_id = db.Column(db.Integer, db.ForeignKey('cities.id'), nullable=False)
-    date = db.Column(db.String(10), nullable=False)
-    temperature = db.Column(db.String(10), nullable=False)
-    humidity = db.Column(db.String(10), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+    humidity = db.Column(db.Float, nullable=False)
     condition = db.Column(db.String(50), nullable=False)
-    wind_speed = db.Column(db.String(10), nullable=False)
+    wind_speed = db.Column(db.Float, nullable=False)
